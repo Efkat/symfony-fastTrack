1bf3dbf8ed8dadb39c6b4b87e76943cfb449abc2
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2023-05-25 21:09:52 +0200

    Implement slugger

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,8 +6,11 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+#[UniqueEntity('slug')]
 class Conference
 {
     #[ORM\Id]
@@ -27,7 +30,7 @@ class Conference
     #[ORM\OneToMany(mappedBy: 'conference', targetEntity: Comment::class, orphanRemoval: true)]
     private Collection $Comments;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     private ?string $slug = null;
 
     public function __construct()
@@ -38,6 +41,13 @@ class Conference
     public function __toString(): string
     {
         return $this->city . ' ' . $this->year;
+    }
+
+    public function computeSlug(SluggerInterface $slugger): void
+    {
+        if(!$this->slug || '-' === $this->slug){
+            $this->slug = (string)$slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getId(): ?int
